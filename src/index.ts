8d671abc20c07dfb57624e1605ed8f23cd81d3b3
Barış DEMİRCİ <demirci.baris38@gmail.com>
2020-12-24 08:53:03 +0300

    🤳 Fix dependency issue

--- src/index.ts
+++ src/index.ts
@@ -6,22 +6,9 @@ const successIcon = stdout ? "✔" : "√";
 const warningIcon = stdout ? "⚠" : "‼";
 const errorIcon = stdout ? "✖" : "×";
 
+const generateDate = (): string => grey(new Date().toLocaleString("en-US", { hour: "numeric", minute: "numeric", second: "numeric", hour12: false }));
+const log = (logs: unknown[], icon: string, color: (text: unknown) => string): void => console.log(generateDate(), color(icon), ...logs);
 export const info = (...logs: unknown[]): void => log(logs, infoIcon, cyan);
 export const success = (...logs: unknown[]): void => log(logs, successIcon, green);
 export const warning = (...logs: unknown[]): void => log(logs, warningIcon, yellow);
 export const error = (...logs: unknown[]): void => log(logs, errorIcon, red);
-
-function log(logs: unknown[], icon: string, color: (text: unknown) => string): void {
-    console.log(generateDate(), color(icon), ...logs);
-}
-
-function generateDate(): string {
-    const date = new Date().toLocaleString("en-US", {
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        hour12: false,
-    });
-    const colored = grey(date);
-    return colored;
-}
