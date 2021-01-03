9bc2b7425a6c1c6748e1cbcc7819c8d267615c0b
Barış DEMİRCİ <demirci.baris38@gmail.com>
2021-01-03 18:32:53 +0300

    🧨 Export utilities

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { cyan, green, grey, red, yellowBright as yellow } from "chalk";
 import { stdout } from "supports-color";
 
-const infoIcon = stdout ? "ℹ" : "i";
-const successIcon = stdout ? "✔" : "√";
-const warningIcon = stdout ? "⚠" : "‼";
-const errorIcon = stdout ? "✖" : "×";
+export const infoIcon = stdout ? "ℹ" : "i";
+export const successIcon = stdout ? "✔" : "√";
+export const warningIcon = stdout ? "⚠" : "‼";
+export const errorIcon = stdout ? "✖" : "×";
 
-const generateDate = (): string => grey(new Date().toLocaleString("en-US", { hour: "numeric", minute: "numeric", second: "numeric", hour12: false }));
-const log = (logs: unknown[], icon: string, color: (text: unknown) => string): void => console.log(generateDate(), color(icon), ...logs);
+export const generateDate = (): string => grey(new Date().toLocaleString("en-US", { hour: "numeric", minute: "numeric", second: "numeric", hour12: false }));
+export const log = (logs: unknown[], icon: string, color: (text: unknown) => string): void => console.log(generateDate(), color(icon), ...logs);
 export const info = (...logs: unknown[]): void => log(logs, infoIcon, cyan);
 export const success = (...logs: unknown[]): void => log(logs, successIcon, green);
 export const warning = (...logs: unknown[]): void => log(logs, warningIcon, yellow);
